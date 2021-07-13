97dabfdb58effd9d9e4ad47215ab64e897cfc1f9
Tinaes <aj.tinaes@gmail.com>
2021-07-13 10:08:42 +0800

    base moves

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // create emoji array
 const emojiArr = ["✌","😂","😝","😁","😱","👉","🙌","🍻","🔥","🌈","☀","🎈","🌹","💄","🎀","⚽","🎾","🏁","😡","👿","🐻","🐶","🐬","🐟","🍀","👀","🚗","🍎","💝","💙","👌","❤","😍","😉","😓","😳","💪","💩","🍸","🔑","💖","🌟","🎉","🌺","🎶","👠","🏈","⚾","🏆","👽","💀","🐵","🐮","🐩","🐎","💣","👃","👂","🍓","💘","💜","👊","💋","😘","😜","😵","🙏","👋","🚽","💃","💎","🚀","🌙","🎁","⛄","🌊","⛵","🏀","🎱","💰","👶","👸","🐰","🐷","🐍","🐫","🔫","👄","🚲","🍉","💛","💚"];
-
+const movesEmojiArr = ["⭐","🌟","✨","🌌","🌙"]
+const moonArr = ["🌑","🌒","🌓","🌔","🌕","🌖","🌗","🌘","🌚"]
+let moonCycle = 0;
 const para = document.createElement('p');
+const movesButton = document.createElement('button');
+const moveGridSize =10;
+movesButton.innerText = "Moves"
+movesButton.addEventListener('click', movesFunction);
+
+let currentPosition = [moveGridSize -1, 0];
 
 for(let i = 0; i < emojiArr.length; i+=1) {
   const button = document.createElement('button');
@@ -9,9 +17,14 @@ for(let i = 0; i < emojiArr.length; i+=1) {
   button.addEventListener('click', () => {buttonPress(emojiArr[i])});
   document.body.appendChild(button);
 }
-document.addEventListener('keydown', fn)
 
-// function
+document.body.appendChild(movesButton);
+document.body.appendChild(para);
+document.body.addEventListener('keypress', (e) => {moveEmoji(e.key)});
+
+
+// functions
+// returns grid of emojis
 function buttonPress(emoji) {
   para.innerText = "";
   for(let j = 0; j < 3; j +=1) {
@@ -21,4 +34,41 @@ function buttonPress(emoji) {
     }
     para.innerHTML += '<br>';
   }
+}
+
+function movesFunction() {
+  para.innerText = "";
+  let emojiSelector = Math.floor(Math.random() * movesEmojiArr.length);
+  for(let i = 0; i < moveGridSize; i +=1) {
+    
+    for (let j = 0; j < moveGridSize; j +=1) {
+      if (currentPosition[0] == i && currentPosition[1] == j) {
+        para.innerText += "🚀";
+      }
+      else {
+         para.innerText += movesEmojiArr[emojiSelector];
+      }
+    }
+    para.innerHTML += '<br>';
+  }
+}
+
+function moveEmoji(arrowKey) {
+  console.log('here');
+  switch (arrowKey) {
+    case "w":
+      currentPosition[0] -=1;
+      break;
+      case "s":
+      currentPosition[0] +=1;
+      break;
+      case "a":
+      currentPosition[1] -=1;
+      break;
+      case "d":
+      currentPosition[1] +=1;
+      break;
+  }
+  console.log(currentPosition);
+  movesFunction();
 }
